38eb2756cb1a6f5451bba5c3340468bb70276857
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-02-26 20:44:15 -0500

    trying to fix my errors in index for reducers

--- my-app/src/reducer/index.js
+++ my-app/src/reducer/index.js
@@ -1,14 +1,23 @@
 export const initialState = {
-	todo: 'List of todo items',
-	editing: false
+	item: 'Learn about reducers',
+	completed: false,
+	id: 3892987589
 };
+
+export const todoList = [
+	{
+		item: '',
+		completed: false,
+		id: ''
+	}
+];
 
 export const todoReducer = (state, action) => {
 	switch (action.type) {
 		case 'CHANGE_TODO':
-			return { ...state, todo: action.payload, editing: false };
+			return { ...state, todo: action.payload, completed: false, id: newDate() };
 		case 'CHANGE_EDIT':
-			return { ...state, editing: true };
+			return { ...state, completed: !item.completed };
 		default:
 			return state;
 	}
